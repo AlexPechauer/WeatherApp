f5479e76a8933ae4076b8da116001b3c473bd2d3
Alex Pechauer <apechaue@gmail.com>
2021-12-04 12:06:07 -0600

    Clean up controller and html/css;

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import got from "got";
 import { config } from "dotenv";
 
 config();
-const apiKey = `${process.env.API_KEY}`;
-const app = express();
+var apiKey = `${process.env.API_KEY}`;
+var app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,43 +17,35 @@ app.get('/', (req, res) => {
 
 app.post('/', async function (req, res) {
 
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  var city = req.body.city;
+  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
   (async () => {
     try {
-      const response = await got(url);
-      const weather = JSON.parse(response.body);
+      var response = await got(url);
+      var weather = JSON.parse(response.body);
 
       if (!weather.main) {
         res.render('index', { weather: null, error: 'Error, please try again' });
       }
 
-      let place = `${weather.name}, ${weather.sys.country}`,
-        weatherTimezone = `${new Date(weather.dt * 1000 - weather.timezone * 1000)}`,
-        weatherTemp: number = +`${weather.main.temp}`,
-        weatherPressure = `${weather.main.pressure}`,
-        weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
-        weatherDescription = `${weather.weather[0].description}`,
-        humidity = `${weather.main.humidity}`,
-        clouds = `${weather.clouds.all}`,
-        visibility = `${weather.visibility}`,
-        main = `${weather.weather[0].main}`,
-        weatherFahrenheit: number = (weatherTemp * 9) / 5 + 32;
+      var weatherTemp = Math.trunc(+`${weather.main.temp}`),
+        weatherFahrenheit = Math.trunc((weatherTemp * 9) / 5 + 32);
 
       res.render("index", {
+        alexDesc: (weatherFahrenheit > 50 ? 'Perfect weather' : 'Too cold'),
         weather: weather,
-        place: place,
-        temp: weatherTemp,
-        pressure: weatherPressure,
-        icon: weatherIcon,
-        description: weatherDescription,
-        timezone: weatherTimezone,
-        humidity: humidity,
-        fahrenheit: weatherFahrenheit,
-        clouds: clouds,
-        visibility: visibility,
-        main: main,
+        place: `${weather.name}, ${weather.sys.country}`,
+        temp: Math.trunc(+`${weather.main.temp}`),
+        pressure: `${weather.main.pressure}`,
+        icon: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+        description: capitalize(`${weather.weather[0].description}`),
+        timezone: `${new Date(weather.dt * 1000 - weather.timezone * 1000)}`,
+        humidity: `${weather.main.humidity}`,
+        fahrenheit: Math.trunc((weatherTemp * 9) / 5 + 32),
+        clouds: `${weather.clouds.all}`,
+        visibility: `${weather.visibility}`,
+        main: `${weather.weather[0].main}`,
         error: null,
       });
     } catch (error) {
@@ -66,3 +58,12 @@ app.post('/', async function (req, res) {
 app.listen(4000, () => {
   console.log(`server running on port 4000`);
 });
+
+function capitalize(text: string) {
+  var words = text.split(' ');
+  var capitalizedWords = [];
+  words.forEach(word => {
+    capitalizedWords.push(word[0].toUpperCase() + word.slice(1, word.length));
+  });
+  return capitalizedWords.join(' ');
+}
